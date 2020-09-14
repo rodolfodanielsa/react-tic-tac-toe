d4740ccfbf2caf33c019dbc53f604e1e9272b3d7
Rodolfo Sa <rodolfo.sa@POLT0063.local>
2020-09-14 13:54:45 +0100

    Basic functionality added

---
+++ src/Components/Board.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react'
+import Square from './Square'
+import {calculateWinner} from '../Helpers/Functions';
+
+export default class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            lastMove: ''
+        };
+    }
+
+    handleClick(i) {
+
+        const squares = this
+            .state
+            .squares
+            .slice();
+        let lastMove = this.state.lastMove;
+
+        let [winner,
+            winnerLine] = calculateWinner(squares);
+
+        if (squares[i] || winner) {
+            return;
+        }
+
+        lastMove = lastMove !== 'X'
+            ? 'X'
+            : 'O';
+        squares[i] = lastMove;
+        this.setState({squares: squares, lastMove: lastMove});
+
+    }
+
+    renderSquare(i) {
+        let line = [];
+        let [winner,
+            winnerLine] = calculateWinner(this.state.squares);
+        if (winner) {
+            console.log('WINNER');
+            console.log(winnerLine);
+            console.log(i);
+            line = winnerLine;
+        }
+        console.log(line.indexOf(i) > 0);
+        console.log(line.indexOf(i));
+        let winClass = line.indexOf(i) >= 0
+            ? 'win'
+            : '';
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+            className={winClass}/>
+    }
+
+    render() {
+        return (
+            <div className='board'>
+                
+                <div className='row'>
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        )
+    }
+}
